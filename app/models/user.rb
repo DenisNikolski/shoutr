1797dd4b860dc78e8cbcda089748108c843b2ccf
DenisNikolski <dzianisnikolski@gmail.com>
2020-07-26 22:42:52 +0300

    Allow user to shout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
+  has_many :shouts, dependent: :destroy
+
   def self.find_user_by_email_or_name(email_or_username)
     User.where(email: email_or_username)
         .or(User.where(username: email_or_username)).first
