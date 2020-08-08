73ceebf95c695bc3104a95a4cb0ef70507ee781e
DenisNikolski <dzianisnikolski@gmail.com>
2020-08-08 13:23:38 +0300

    Add search by photo shout image name

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,7 +4,24 @@ class ShoutSearchQuery
   end
 
   def by_term
-    Shout.joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
-         .where("concat(text_shouts.body,' ') LIKE ?", "%#{@term} %")
+    shouts_for_text_shouts.or(shouts_for_photo_shouts)
+  end
+
+  private
+
+  def shouts_for_text_shouts
+    Shout.where(content_type: 'TextShout', content_id: matching_text_shouts)
+  end
+
+  def shouts_for_photo_shouts
+    Shout.where(content_type: 'PhotoShout', content_id: matching_photo_shouts)
+  end
+
+  def matching_text_shouts
+    TextShout.where("concat(text_shouts.body,' ') LIKE ?", "%#{@term} %")
+  end
+
+  def matching_photo_shouts
+    PhotoShout.where('image_file_name LIKE ?', "%#{@term}%")
   end
 end
